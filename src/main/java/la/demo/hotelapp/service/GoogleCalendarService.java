a9854035505dabf9e7ceccb557b67816c342e501
Deniz Durmus <denizdurmus87@yandex.com>
2019-04-21 19:37:54 +0800

    google cal insertion handler

--- src/main/java/la/demo/hotelapp/service/GoogleCalendarService.java
+++ src/main/java/la/demo/hotelapp/service/GoogleCalendarService.java
@@ -1,15 +1,44 @@
 package la.demo.hotelapp.service;
 
 
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
+import la.demo.hotelapp.entity.Guest;
+import la.demo.hotelapp.entity.Room;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class GoogleCalendarService {
 
 
 
-    public void addCalendarEvent(){
-        //TODO - Stub
+    public void addCalendarEvent(
+            List<Guest> guests,
+            Calendar calendarClient,
+            LocalDate checkIn,
+            LocalDate checkout,
+            Room bookedRoom) throws IOException {
+
+        List<EventAttendee> guestList = new ArrayList<>();
+
+        guests.stream().forEach(guest -> guestList.add(new EventAttendee().setDisplayName(guest.getFullname()).setEmail(guest.getEmail())));
+
+        Event roomBooking = new Event().setSummary("Reservation").setLocation(bookedRoom.toString()).setAttendees(guestList);
+        //TODO check the conversion formats
+        roomBooking.setStart(new EventDateTime().setDateTime(DateTime.parseRfc3339(checkIn.toString())));
+        roomBooking.setEnd(new EventDateTime().setDateTime(DateTime.parseRfc3339(checkout.toString())));
+
+        calendarClient.events().insert("primary", roomBooking).execute();
+
+
     }
 
 
