fe14e58a3baef8b8de629502898f4ac67159dcfe
Deniz Durmus <denizdurmus87@yandex.com>
2019-04-25 20:14:53 +0800

    adding room code for room selection

--- src/main/java/la/demo/hotelapp/util/RoomBookRequest.java
+++ src/main/java/la/demo/hotelapp/util/RoomBookRequest.java
@@ -10,11 +10,21 @@ public class RoomBookRequest {
     private LocalDate checkInDay;
     private LocalDate checkOutDay;
     private List<Guest> guests;
+    private String roomCode;
 
-    public RoomBookRequest(LocalDate checkInDay, LocalDate checkOutDay, List<Guest> guests) {
+    public RoomBookRequest(LocalDate checkInDay, LocalDate checkOutDay, List<Guest> guests, String roomCode) {
         this.checkInDay = checkInDay;
         this.checkOutDay = checkOutDay;
         this.guests = guests;
+        this.roomCode = roomCode;
+    }
+
+    public String getRoomCode() {
+        return roomCode;
+    }
+
+    public void setRoomCode(String roomCode) {
+        this.roomCode = roomCode;
     }
 
     public LocalDate getCheckInDay() {
