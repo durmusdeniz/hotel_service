f46907f653a242bdfc51e0f163be3837b3c85cdd
Deniz Durmus <denizdurmus87@yandex.com>
2019-04-26 19:06:05 +0800

    api descriptions for documentation

--- src/main/java/la/demo/hotelapp/controller/HotelController.java
+++ src/main/java/la/demo/hotelapp/controller/HotelController.java
@@ -1,6 +1,7 @@
 package la.demo.hotelapp.controller;
 
 
+import io.swagger.annotations.ApiOperation;
 import la.demo.hotelapp.entity.Room;
 import la.demo.hotelapp.service.HotelService;
 import la.demo.hotelapp.util.RoomBookRequest;
@@ -23,22 +24,26 @@ public class HotelController {
 
 
     @PostMapping("/bookwithroom")
+    @ApiOperation(value="Makes a room booking with the selected room from the ui")
     public ResponseEntity<Set<Room>> makeBookingV2(@RequestBody RoomBookRequest bookingRequest){
         return ResponseEntity.ok(hotelService.bookRoomV2(bookingRequest));
     }
 
 
     @PostMapping("/listemptyrooms")
+    @ApiOperation(value="Lists the empty rooms for the check-in and check-out dates")
     public ResponseEntity<Set<Room>> listAvaiableRooms(@RequestBody RoomCheckRequest roomCheckRequest){
         return ResponseEntity.ok(hotelService.getEmptyRooms(roomCheckRequest));
     }
 
     @PostMapping("/bookroom")
+    @ApiOperation(value="Makes a room booking with a random room based on the occupant amount")
     public ResponseEntity<Boolean> makeBooking(@RequestBody RoomBookRequest bookingRequest){
         return ResponseEntity.ok(hotelService.bookRoom(bookingRequest));
     }
 
     @PostMapping("/listrooms")
+    @ApiOperation(value="Lists all of the rooms for the selected dates")
     public ResponseEntity<Set<Room>> listAllRooms(@RequestBody RoomCheckRequest roomCheckRequest){
         return ResponseEntity.ok(hotelService.getAllRooms(roomCheckRequest));
     }
